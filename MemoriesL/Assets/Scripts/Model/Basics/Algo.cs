fdfc0938757460e74b9a946a0de671e681f49681
Fabien DOMPEY <dompeydnf@gmail.com>
2021-08-16 11:46:30 +0200

    DungeonInit

---
+++ MemoriesL/Assets/Scripts/Model/Basics/Algo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Algo
+{
+
+    public static List<DungeonStage> GenerateStages(int numberofStages)
+    {
+        List<DungeonStage> result = new List<DungeonStage>();
+        for(int i = 0; i < numberofStages; i++)
+        {
+
+        }
+        return result;
+    }
+}
---
+++ MemoriesL/Assets/Scripts/Model/Dungeon/Dungeon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*  Dungeon's class which represents every Dungeon in which the player will play
+ * Author : DOMPEY Fabien
+ * Date : 16/08/2021
+ */
+public class Dungeon
+{
+    private Difficulty Difficulty { get; }
+
+    private List<DungeonStage> Stages { get;  }
+
+    private int CompletionRate { get; set; }
+
+    public Dungeon(Difficulty d)
+    {
+        int numberofStages = 1;
+        Difficulty = d;
+        switch (d)
+        {
+            case Difficulty.Easy:
+                numberofStages = 2;
+                break;
+            case Difficulty.Medium:
+                numberofStages = 4;
+                break;
+            case Difficulty.Hard:
+                numberofStages = 8;
+                break;
+            case Difficulty.VeryHard:
+                numberofStages = 16;
+                break;
+        }
+        CompletionRate = 0;
+        Stages = new List<DungeonStage>(Algo.GenerateStages(numberofStages));
+    }
+
+
+}
