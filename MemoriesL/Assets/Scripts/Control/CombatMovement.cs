5dda70fff903f3d9e397499fa0f746c74f92a1f9
Fabien DOMPEY <dompeydnf@gmail.com>
2021-08-25 14:33:03 +0200

    ResourcesArranged

---
+++ MemoriesL/Assets/Scripts/Control/CombatMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatMovement : MonoBehaviour
+{
+    public GameObject sprite;
+    private Animator anim;
+    private string path;
+
+    public CombatMovement(string path)
+    {
+        this.path = path;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = sprite.GetComponent<Animator>();
+        anim.runtimeAnimatorController = Resources.Load("Controllers/"+path) as RuntimeAnimatorController;
+    }
+}
